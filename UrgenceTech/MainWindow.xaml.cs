24ebbc127be5054f9c29f342b2947bfb67bba98c
Adam Roy <2387907@collegemv.qc.ca>
2026-03-25 18:08:42 -0400

    Background corrigé

--- UrgenceTech/MainWindow.xaml.cs
+++ UrgenceTech/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UrgenceTech.Views;
 
 namespace UrgenceTech
 {
@@ -19,6 +20,7 @@ namespace UrgenceTech
         public MainWindow()
         {
             InitializeComponent();
+            MainFrame.Navigate(new SignIn());
         }
     }
 }
