eee4dc7425f22dbe51beeb4a5dfe7da4830a9172
Emile <emiletardificloud@icloud.com>
2026-03-25 22:49:34 -0400

    Code fini sprint 1

--- UrgenceTech/App.xaml.cs
+++ UrgenceTech/App.xaml.cs
@@ -1,14 +1,33 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Navigation;
+using UrgenceTech.Views;
 
 namespace UrgenceTech
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    /// Logique d'interaction pour App.xaml
     /// </summary>
     public partial class App : Application
     {
-    }
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            NavigationWindow window = new NavigationWindow();
+            window.Source = new Uri("views/SignUp.xaml", UriKind.Relative);
+            window.Show();
+        }
+
+        internal void Show()
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
