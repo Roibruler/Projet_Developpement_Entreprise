0b5764d1539790ce4a66eda2b00461d69d9ac6bc
Adam Roy <2387907@collegemv.qc.ca>
2026-03-25 21:02:55 -0400

    Validation ajoutée.

--- UrgenceTech/Views/SignIn.xaml.cs
+++ UrgenceTech/Views/SignIn.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -27,12 +28,64 @@ namespace UrgenceTech.Views
 
         private void SignInBtn_Click(object sender, RoutedEventArgs e)
         {
+            MessageErreur.Text = string.Empty;
 
+            if (!IsEmailValid()) 
+            { 
+                return; 
+            }
+
+            if (!IsPasswordValid())
+            {
+                return;
+            }
+
+            NavigationService.Navigate(new Test()); // Aller à la page d'accueil après une connexion réussie
         }
 
         private void GoToSignUpBtn_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private bool IsEmailValid()
+        {
+            if (string.IsNullOrEmpty(Email.Text) || !IsValidEmailFormat())
+            {
+                MessageErreur.Text = "Le courriel est invalide.";
+                return false;
+            }
+
+            if (Email.Text.Length > 100)
+            {
+                MessageErreur.Text = "Le courriel est trop long.";
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool IsPasswordValid()
+        {
+            if (string.IsNullOrEmpty(MotPasse.Password))
+            {
+                MessageErreur.Text = "Le mot de passe est invalide.";
+                return false;
+            }
+
+            if (MotPasse.Password.Length > 50)
+            {
+                MessageErreur.Text = "Le mot de passe est trop long.";
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool IsValidEmailFormat()
+        {
+            string pattern = @"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$";
+            return Regex.IsMatch(Email.Text, pattern, RegexOptions.IgnoreCase);
         }
     }
 }
