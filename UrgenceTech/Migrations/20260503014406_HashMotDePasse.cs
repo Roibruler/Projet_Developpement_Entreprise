bcc6e110cb85f7928ffdcb4ca8834b4ac660a750
Roibruler <adamroy7428@gmail.com>
2026-05-02 21:57:35 -0400

    Ajout tableau de bord et corrections sécurité

---
+++ UrgenceTech/Migrations/20260503014406_HashMotDePasse.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UrgenceTech.Migrations
+{
+    /// <inheritdoc />
+    public partial class HashMotDePasse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Utilisateurs",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "MotDePasse",
+                value: "$2a$11$.wa0BmRx22LSyp.mhLsvh.0d8OTKAkqsnmnKuDFXff3ajaXPpfE.G");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Utilisateurs",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "MotDePasse",
+                value: "admin123");
+        }
+    }
+}
