5e040548fe7b0bdb11bbe89d9b1962154fd70bab
Imranechk <Imranechkhit52@gmail.com>
2026-05-05 14:59:39 -0400

    Statistiques tableau de bord et consultation urgences

---
+++ UrgenceTech/Repositories/UrgenceRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UrgenceTech.Data;
+using UrgenceTech.Models;
+
+namespace UrgenceTech.Repositories
+{
+    internal class UrgenceRepository : IUrgenceRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UrgenceRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Urgences avec leur utilisateur
+        public async Task<List<Urgence>> ObtenirToutesAsync()
+        {
+            return await _context.Urgences
+                .Include(u => u.Utilisateur)
+                .OrderByDescending(u => u.DateCreation)
+                .ToListAsync();
+        }
+
+        // Urgences filtrées par statut
+        public async Task<List<Urgence>> ObtenirParStatutAsync(string statut)
+        {
+            return await _context.Urgences
+                .Include(u => u.Utilisateur)
+                .Where(u => u.Statut == statut)
+                .OrderByDescending(u => u.DateCreation)
+                .ToListAsync();
+        }
+
+        // Urgences filtrées par priorité
+        public async Task<List<Urgence>> ObtenirParPrioriteAsync(string priorite)
+        {
+            return await _context.Urgences
+                .Include(u => u.Utilisateur)
+                .Where(u => u.Priorite == priorite)
+                .OrderByDescending(u => u.DateCreation)
+                .ToListAsync();
+        }
+
+    }
+}
---
+++ UrgenceTech/Views/UrgenceView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UrgenceTech.ViewModels;
+
+namespace UrgenceTech.Views
+{
+    public partial class UrgenceView : UserControl
+    {
+        public UrgenceView()
+        {
+            InitializeComponent();
+            DataContext = new UrgenceViewModel();
+        }
+    }
+}
